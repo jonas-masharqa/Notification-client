591322299ede4e2c1d00a67a425bf9fc4354b401
jonas <jonas.masharqa@gmail.com>
2020-01-23 23:04:45 +0100

    Emit and input
     -Adds onChange input field with setMessage hook
     -Adds onClick button for sendMessage function
     -Adds sendMessage function, emitting newMessage to chat channel

--- src/Components/WriteMessage.jsx
+++ src/Components/WriteMessage.jsx
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
-import openSocket from 'socket.io-client';
+import React, { useState } from 'react'
+import openSocket from 'socket.io-client'
 
 const WriteMessage = () => {
   const [newMessage, setMessage] = useState('')
 
-  const socket = openSocket('http://localhost:5000');
+  const socket = openSocket('http://localhost:5000')
+
+  const sendMessage = () => {
+    console.log('MESSAGE SENT')
+    socket.emit('chat', newMessage);
+    setMessage('')
+  }
+
   return (
     <>
+      <div>
+        <h2>Write Your Message!</h2>
+        <input
+          onChange={e => setMessage(e.target.value)}
+          placeholder="Your messages goes here .."
+        />
+        <button onClick={() => sendMessage()}>↪</button>
+      </div>
     </>
   )
 }
