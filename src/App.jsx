afab78480ec9deea6f01c2dbd3bb298e4e2d44a7
jonas <jonas.masharqa@gmail.com>
2020-01-23 21:23:14 +0100

    Routing
     -Changes App to functional component
     -Adds react router dom
     -Wraps app in BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        
-      </>
-    )
-  }
+const App = () => {
+  return (
+    <>
+    </>
+  )
 }
 
 export default App
