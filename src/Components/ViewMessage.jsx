f93c440af58059c87835973bbbf9801f35ebabb8
jonas <jonas.masharqa@gmail.com>
2020-01-23 23:16:09 +0100

    Adds allMessages state and ws server connection.

--- src/Components/ViewMessage.jsx
+++ src/Components/ViewMessage.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 const ViewMessage = () => {
+  const [allMessages, setAllMessages] = useState([])
+
+  const socket = openSocket('http://localhost:5000');
+  
   return (
     <>
     </>
