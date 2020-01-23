90bc085275d011d654f6cb885571511111dbd5ff
jonas <jonas.masharqa@gmail.com>
2020-01-23 21:36:35 +0100

    Creates routes for all components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import LandingPage from './Components/LandingPage'
+import WriteMessage from './Components/WriteMessage'
+import ViewMessage from './Components/ViewMessage'
+import { Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
+      <Route exact path="/" component={LandingPage} />
+      <Route exact path="/write" component={WriteMessage} />
+      <Route exact path="/view" component={ViewMessage} />
     </>
   )
 }
