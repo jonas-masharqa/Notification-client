3be0d256caf870e948a6cb649dcbc1913290d6f3
jonas <jonas.masharqa@gmail.com>
2020-01-23 23:23:59 +0100

    Displaying channel messages
    -Adds new messages from channel in allMessages state.
    -Maps over messages from state to variable, renders variable.
    -Imports useState and openSocket

--- src/Components/ViewMessage.jsx
+++ src/Components/ViewMessage.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import openSocket from 'socket.io-client'
 
 const ViewMessage = () => {
   const [allMessages, setAllMessages] = useState([])
 
-  const socket = openSocket('http://localhost:5000');
-  
+  let message
+
+  const socket = openSocket('http://localhost:5000')
+
+  if (allMessages) {
+    message = allMessages.map(message => {
+      return <div>{message}</div>
+    })
+  }
+
+  socket.on('chat', (data) => {
+    setAllMessages([...allMessages, data])
+  })
+
   return (
     <>
+      <div>
+        {message}
+      </div>
     </>
   )
 }
