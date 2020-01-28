76b11d816c1cacb271eaee14e270cb27fa1536c1
jonas <jonas.masharqa@gmail.com>
2020-01-28 19:06:46 +0100

    Adds syling for input field.

--- src/Components/WriteMessage.jsx
+++ src/Components/WriteMessage.jsx
@@ -1,32 +1,42 @@
 import React, { useState } from 'react'
 import openSocket from 'socket.io-client'
-import { Header, Icon, Container, Grid } from 'semantic-ui-react'
+import NeonLines from '../Images/neon-lines.jpg'
+import { Button, TextArea } from 'semantic-ui-react'
 
 const WriteMessage = () => {
+  const lines = <img id="lines-pic" src={NeonLines} alt="Neon Lines Picture" />
+
   const [newMessage, setMessage] = useState('')
 
   const socket = openSocket('https://jonas-message-backend.herokuapp.com/')
 
   const sendMessage = () => {
     console.log('MESSAGE SENT')
-    socket.emit('chat', newMessage);
+    socket.emit('chat', newMessage)
     setMessage('')
   }
 
   return (
     <>
-      <div>
-        <h2>Write Your Message!</h2>
-        <input
-          onChange={e => setMessage(e.target.value)}
-          value={newMessage}
-          placeholder="Your messages goes here .."
-        />
-        <button onClick={() => sendMessage()}>↪</button>
-      </div>
+      <center>
+        <div id="write-container">
+          <div id="lines-pic">{lines}</div>
+          <div id="input">
+            <div>
+              <h1>Write Your Message!</h1>
+              <TextArea
+                onChange={e => setMessage(e.target.value)}
+                value={newMessage}
+                placeholder="Your messages goes here .."
+                id="text-area"
+              />
+            </div>
+            <Button onClick={() => sendMessage()}>Send</Button>
+          </div>
+        </div>
+      </center>
     </>
   )
 }
 
 export default WriteMessage
-
